a205e385985124c03eda824693e5ea2ab95d96ee
EdgarDjj <472484327@qq.com>
2021-04-01 11:12:08 +0800

    kafka change

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,8 +4,11 @@ import (
 	"bytes"
 	"collector/pkg/entities"
 	"collector/pkg/registry"
+	"flag"
 	"fmt"
 	"github.com/Shopify/sarama"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"log"
 	"os"
 	"os/signal"
@@ -23,7 +26,15 @@ var (
 var c = GetConfig()
 
 func main() {
+	command := newCollectorCommand()
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	// load the IPFIX global registry
+	log.Println("Starting IPFIX collector!")
 	registry.LoadRegistry()
 	// load config
 	IPFIXAddr = c.udpConfig.Host
@@ -47,7 +58,7 @@ func main() {
 
 	cp, err := InitCollectingProcess(cpInput)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	// 监听连接与接受消息
 	messageReceived := make(chan *entities.Message)
@@ -55,6 +66,7 @@ func main() {
 		go cp.Start()
 		msgChan := cp.GetMsgChan()
 		for message := range msgChan {
+			log.Println("Processing IPFIX message sucessfully!")
 			messageReceived <- message
 		}
 	}()
@@ -63,6 +75,30 @@ func main() {
 
 	<-stopCh
 	cp.Stop()
+	log.Println("Stopping IPFIX collector!")
+	return nil
+}
+
+func newCollectorCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:  "ipfix-collector",
+		Long: "IPFIX collector to decode the exported flow records",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := run(); err != nil {
+				log.Fatalf("Error when running IPFIX collector: %v", err)
+			}
+		},
+	}
+	flags := cmd.Flags()
+	addIPFIXFlags(flags)
+	flags.AddGoFlagSet(flag.CommandLine)
+	return cmd
+}
+
+func addIPFIXFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&IPFIXAddr, "ipfix.addr", "0.0.0.0", "IPFIX collector address")
+	fs.Uint16Var(&IPFIXPort, "ipfix.port", 4739, "IPFIX collector port")
+	fs.StringVar(&IPFIXTransport, "ipfix.transport", "udp", "IPFIX collector transport layer")
 }
 
 func signalHandler(stopCh chan struct{}, messageReceived chan *entities.Message) {
@@ -84,40 +120,58 @@ func signalHandler(stopCh chan struct{}, messageReceived chan *entities.Message)
 // 三种连接kafka方式 1、无认证 2、TLS认证 3、SASL/PLAIN
 func sendMessageByKafka(buf bytes.Buffer) {
 	topic := c.kafkaConfig.Topic
-	producer, err := newSyncProducer()
+	kafkaHost := c.kafkaConfig.Host
+	kafkaPort := c.kafkaConfig.Port
+	addr := strings.Join([]string{kafkaHost, strconv.Itoa(kafkaPort)}, ":")
+	brokerList := []string{addr}
+
+	kafkaProducer, err := newSyncProducer(brokerList, topic, true)
+
 	log.Printf("$$$ start send Message to Kafka, the topic is %s\n", topic)
 	if err != nil {
 		log.Fatalln("Failed to start start Sarama producer: ", err)
 		return
 	}
+
 	msgToSend := buf.Bytes()
-	messageSend := &sarama.ProducerMessage{
-		Topic: topic,
+
+	kafkaProducer.producer.Input() <- &sarama.ProducerMessage{
+		Topic: kafkaProducer.topic,
 		Value: sarama.ByteEncoder(msgToSend),
-	}
-	pid, offset, err := producer.SendMessage(messageSend)
-	if err != nil {
-		log.Fatalln("send message Failed!")
-	} else {
-		log.Printf("Message send: %s pid: %v offset: %v", messageSend, pid, offset)
 	}
 }
 
-func newSyncProducer() (sarama.SyncProducer, error) {
-	kafkaHost := c.kafkaConfig.Host
-	kafkaPort := c.kafkaConfig.Port
-	addr := strings.Join([]string{kafkaHost, strconv.Itoa(kafkaPort)}, ":")
-	brokerList := []string{addr}
-	config := sarama.NewConfig()
+type KafkaProducer struct {
+	producer sarama.AsyncProducer
+	topic    string
+}
 
-	config.Producer.Return.Successes = true
-	producer, err := sarama.NewSyncProducer(brokerList, config)
+func newSyncProducer(addrs []string, topic string, logErrors bool) (*KafkaProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = false
+
+	asyncProducer, err := sarama.NewAsyncProducer(addrs, config)
 	if err != nil {
 		return nil, err
 	}
-	return producer, err
+	producer := NewKafkaProducer(asyncProducer, topic)
+
+	if logErrors {
+		go func() {
+			for msg := range asyncProducer.Errors() {
+				log.Fatalln(msg)
+			}
+		}()
+	}
+	return producer, nil
 }
 
+func NewKafkaProducer(asyncProducer sarama.AsyncProducer, topic string) *KafkaProducer {
+	return &KafkaProducer{
+		producer: asyncProducer,
+		topic:    topic,
+	}
+}
 func printIPFIXMessage(msg *entities.Message) bytes.Buffer {
 	var buf bytes.Buffer
 	fmt.Fprint(&buf, "\nIPFIX-HDR:\n")
@@ -144,6 +198,6 @@ func printIPFIXMessage(msg *entities.Message) bytes.Buffer {
 		}
 	}
 	fmt.Println(buf.String())
-	fmt.Println("$$$ Data:", buf.Bytes())
+	//fmt.Println("$$$ Data:", buf.Bytes())
 	return buf
 }
